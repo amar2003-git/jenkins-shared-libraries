4b5282960b80949ee21e004bef431f64f38649f5
amar2003-git <amarkumarp351@gmail.com>
2026-01-05 08:37:36 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,5 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
-    sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
-    """
+def call(string imageTag, string dockerHubuser){
+    sh "docker build -t ${dockerHubuser}/$(projectname}:${image tag} ."
 }
+    
+ 
